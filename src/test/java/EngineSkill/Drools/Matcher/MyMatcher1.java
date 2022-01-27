7d494ce0aef4b0fc71567dd395d67ac448260ac7
fuyiqi <864659174@qq.com>
2022-01-27 17:14:49 +0800

    变更

--- src/test/java/EngineSkill/Drools/Matcher/MyMatcher1.java
+++ src/test/java/EngineSkill/Drools/Matcher/MyMatcher1.java
@@ -6,6 +6,9 @@ public class MyMatcher1 {
 
     private static DroolsHelper droolsHelper = DroolsHelper.getInstance("MyMatcher1");
 
-
+    public static void main(String[] args) {
+        String[] res = MyMatcher1.class.getName().toString().split("\\.");
+        System.out.println(res[res.length-1]);
+    }
 
 }
