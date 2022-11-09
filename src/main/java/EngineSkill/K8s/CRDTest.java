49890437ffdeec83987c3fef2763a3c45e7df2fa
macbook <864659174@qq.com>
2022-11-09 21:42:52 +0800

    自定义资源crd（FlinkDeployment）的操纵方法

--- src/main/java/EngineSkill/K8s/CRDTest.java
+++ src/main/java/EngineSkill/K8s/CRDTest.java
@@ -1,43 +1,22 @@
 package EngineSkill.K8s;
 
-import EngineSkill.K8s.crds.FlinkDeployment;
+
+import io.fabric8.kubernetes.api.model.GenericKubernetesResource;
+import io.fabric8.kubernetes.api.model.GenericKubernetesResourceList;
 import io.fabric8.kubernetes.api.model.NamespaceList;
-import io.fabric8.kubernetes.api.model.ObjectMeta;
-import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
-import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionList;
+
 import io.fabric8.kubernetes.client.*;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 
 
 public class CRDTest {
     private static final Logger log= LoggerFactory.getLogger(CRDTest.class);
-
-    public static String basicYaml = "apiVersion: flink.apache.org/v1beta1\n" +
-            "kind: FlinkDeployment\n" +
-            "metadata:\n" +
-            "  name: basic-example\n" +
-            "spec:\n" +
-            "  image: flink:1.15\n" +
-            "  flinkVersion: v1_15\n" +
-            "  flinkConfiguration:\n" +
-            "    taskmanager.numberOfTaskSlots: \"2\"\n" +
-            "  serviceAccount: flink\n" +
-            "  jobManager:\n" +
-            "    resource:\n" +
-            "      memory: \"2048m\"\n" +
-            "      cpu: 1\n" +
-            "  taskManager:\n" +
-            "    resource:\n" +
-            "      memory: \"2048m\"\n" +
-            "      cpu: 1\n" +
-            "  job:\n" +
-            "    jarURI: local:///opt/flink/examples/streaming/StateMachineExample.jar\n" +
-            "    parallelism: 2\n" +
-            "    upgradeMode: stateless";
-
 
     public static void main(String[] args) {
         Config config = new ConfigBuilder()
@@ -51,7 +30,7 @@ public class CRDTest {
                         //log.info(namespace.getMetadata().getName()+":"+namespace.getStatus().getPhase());
                     }
             );
-            //自定义资源pojo
+            //crd的关键属性,也可以定义成实体类
             CustomResourceDefinitionContext context = new CustomResourceDefinitionContext
                     .Builder()
                     .withName("flinkdeployments.flink.apache.org")
@@ -61,26 +40,21 @@ public class CRDTest {
                     .withVersion("v1beta1")
                     .withPlural("flinkdeployments")
                     .build();
-            //获取集群crd列表
-            CustomResourceDefinitionList crdList = client.apiextensions().v1().customResourceDefinitions().list();
-            String flinkDeploymentCRDName = CustomResource.getCRDName(FlinkDeployment.class);
-            CustomResourceDefinition flinkDeploymentCRD = null;
-            for (CustomResourceDefinition crd :crdList.getItems()){
-                ObjectMeta metadata = crd.getMetadata();
-                if (metadata != null){
-                    String name = metadata.getName();
-                    if(flinkDeploymentCRDName.equals(name)){
-                        flinkDeploymentCRD = crd;
-                    }
-                }
-            }
+            //crd操作引擎
+            MixedOperation<
+                    GenericKubernetesResource,
+                    GenericKubernetesResourceList,
+                    Resource<GenericKubernetesResource>
+                    > resourceMixedOperation =  client.genericKubernetesResources(context);
+            //之后就与正常k8s资源操作api一致，此处是创建
+            resourceMixedOperation
+                    .inNamespace("default")
+                    .load(new File("src/main/resources/k8s/a.yaml"))
+                    .createOrReplace();
 
         }catch (Exception e){
             log.error("Exception:\t",e);
         }
-
-
-
 
 
 
